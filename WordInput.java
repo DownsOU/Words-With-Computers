2dd6774b712b6963389d4c62147b3b8cd0fff177
Mahmudur <mrahman4@oakland.edu>
2020-09-30 11:52:38 -0400

    Create WordInput.java

---
+++ WordInput.java
@@ -0,0 +1,27 @@
+package com.mycompany.words_with_computers;
+import java.util.Scanner;
+
+
+public class WordInput {
+    
+    private String word;
+    private char[] wordChars;
+
+    public WordInput() {
+    }
+      
+    public void letPlayerInput(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter player word: ");
+        this.word = scan.nextLine();
+    } 
+    public String getPlayerInput() {
+        return word;
+    }
+
+    public char[] getPlayerInputChars() {
+        return word.toCharArray();
+    }
+    
+       
+}
