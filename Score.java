c7a95bfd73556233378fa003227fe3545b3e3093
DownsOU <ddowns@oakland.edu>
2020-10-14 10:54:03 -0400

    update

--- Score.java
+++ Score.java
@@ -1,16 +1,28 @@
 package words_with_computers;
 
 public class Score{
-
+    
+//    private String pWrd;
+//    private String cpuWrd;
     private static int pScore;
     private static int cpuScore;
     private String winner;
     
-    public Score(){
-    //Initializing score variables to 0 when a new class instance is created
-        pScore = 0;
-        cpuScore = 0;
-    }
+//    public Score(String playerWord, String computerWord, int playerScore, int computerScore, String win){
+//        pWrd = playerWord;
+//        cpuWrd = computerWord;
+//        pScore = playerScore;
+//        cpuScore = computerScore;
+//        winner = win;
+//    }
+    
+//    public String getPlayerWord(){
+//        return pWrd;
+//    }
+//    
+//    public String getCPUWord(){
+//        return cpuWrd;
+//    }
     
     public int getPlayerScore() {
         return pScore;
@@ -23,45 +35,44 @@ public class Score{
     private int calculateWordScore(String wrd, int score){
         char[] letters = new char[wrd.length()];
         for (int i = 0; i < wrd.length(); i++) { 
-            letters[i] = wrd.charAt(i); 
+            letters[i] = wrd.charAt(i);
         }
         int wrdScore = score;
         for(int i = 0; i < wrd.length(); i++){
             switch (letters[i]){
                 //letter is common if there are 6 or more total
-                case 'a':
-                case 'e':
-                case 'i':
-                case 'n':
-                case 'o':
-                case 'r':
-                case 't':
+                case 'A':
+                case 'E':
+                case 'I':
+                case 'N':
+                case 'O':
+                case 'R':
+                case 'T':
                     wrdScore += 1;
                     break;
                 //letter is uncommon if there are 3-5 total
-                case 'd':
-                case 'g':
-                case 'l':
-                case 's':
-                case 'u':
+                case 'D':
+                case 'G':
+                case 'L':
+                case 'S':
+                case 'U':
                     wrdScore += 3;
                     break;
                 //letter is rare if there are 2 or less total
-                case 'b':
-                case 'c':
-                case 'f':
-                case 'h':
-                case 'j':
-                case 'k':
-                case 'm':
-                case 'p':
-                case 'q':
-                case 'v':
-                case 'w':
-                case 'x':
-                case 'y':
-                case 'z':
-                case ' ':
+                case 'B':
+                case 'C':
+                case 'F':
+                case 'H':
+                case 'J':
+                case 'K':
+                case 'M':
+                case 'P':
+                case 'Q':
+                case 'V':
+                case 'W':
+                case 'X':
+                case 'Y':
+                case 'Z':
                     wrdScore += 5;
             }
         }
@@ -91,8 +102,8 @@ public class Score{
     
     public String endGameResults(){
         determineWinner();
-        String endResults;
-        endResults = "Your score: " + pScore + "\nComputer score: " + cpuScore + "\n" + winner;
-        return endResults;
+        String results;
+        results = "Your score: " + pScore + "\nComputer score: " + cpuScore + "\n" + winner;
+        return results;
     }
 }
