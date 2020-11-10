a8ef349efc719e4fd78b6ec30055d3d0493a3107
Mahmudur <mrahman4@oakland.edu>
2020-11-10 12:37:44 -0500

    Create BoardPlacementWithoutAdjacent.java
    Testing method to prevent adjacent words being placed by pc player.

---
+++ BoardPlacementWithoutAdjacent.java
@@ -0,0 +1,63 @@
+package com.mycompany.Words_With_Computers;
+
+public class BoardPlacementWithoutAdjacent {
+    
+    private BoardModel board;
+
+    public BoardPlacement(BoardModel model) {
+        board = model;
+    }
+
+    public void placePlayer(String word, int row, int column, String orientation) {
+        //check if tiles are clear to place letters
+        for (int i = 0; i < word.length(); i++) {
+            if (orientation.equalsIgnoreCase("H")) {
+                if (board.getTileRight(row, column + i) != ' ' 
+                        && board.getTileRight(row, column + i) != word.charAt(i)) {
+                    System.out.println("Cannot place here");
+                    return;
+                }
+            }
+            if (orientation.equalsIgnoreCase("V")) {
+                if (board.getTileDown(row + i, column) != ' '
+                        && board.getTileDown(row + i, column) != word.charAt(i)) {
+                    System.out.println("Cannot place here");
+                    return;
+                }
+            }
+        }
+        //place letters on board
+        if(orientation.equalsIgnoreCase("H")) {
+            for(int i = 0; i < word.length(); i++){
+                 board.setTile(row, column + i, word.charAt(i));
+            }
+        }
+        if(orientation.equalsIgnoreCase("V")) {
+            for(int i = 0; i < word.length(); i++){
+                 board.setTile(row + i, column, word.charAt(i));
+            }
+        }
+    }
+    
+    public void placeComputer(String word) {
+        for(int i = 0; i < board.getBoard().length; i++) {
+            for( int j = 0; j < board.getBoard().length; j++) {
+                if(word.contains(Character.toString(board.getTile(i,j)))){
+                    if(board.getTileLeft(i,j) == ' ' && board.getTileRight(i,j) == ' ' && board.adjacentClearHorizontal(i, j - word.indexOf(board.getTile(i,j)), j, word.length()) == true) {
+                        for(int k =0; k < word.length(); k++) {
+                            board.setTile(i, j - word.indexOf(board.getTile(i,j)) + k, word.charAt(k));
+                        }
+                        return;
+                    }
+                    if(board.getTileUp(i,j) == ' ' && board.getTileDown(i,j) == ' ' && board.adjacentClearVertical(j, i - word.indexOf(board.getTile(i,j)), i, word.length()) == true) {
+                        for(int k =0; k < word.length(); k++) {
+                            board.setTile(i - word.indexOf(board.getTile(i,j)) + k, j, word.charAt(k));
+                        }
+                        return;
+                    }
+                } 
+            }
+        }
+    }
+
+}
